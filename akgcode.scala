9a7747cbb60577ea0abc018631a1b0d23cf8b4df
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-07-18 22:40:34 +0200

    more stuff

--- akgcode.scala
+++ akgcode.scala
@@ -1,4 +1,4 @@
-// Akshaal, 2017
+// Evgeny "Akshaal" Chukreev, 2017
 
 import scala.util.Try
 import org.apache.commons.io.IOUtils
@@ -6,6 +6,8 @@ import org.rogach.scallop.{ScallopConf, ScallopOption}
 import org.rogach.scallop.exceptions.RequiredOptionNotFound
     
 object AkScriptUtils {
+    var verbose: Boolean = false
+    
     def captureOutputInheritInput(cmd: String, args: String*): String = {
         IOUtils.toString(
             new ProcessBuilder((Seq(cmd) ++ args): _*)
@@ -14,8 +16,40 @@ object AkScriptUtils {
                 .getInputStream(),
             "utf8")
     }
+
+    val isConsole: Boolean = System.console() != null
+    
+    def inConsoleOrElse[T](v: T, vElse: T): T = if (isConsole) v else vElse
     
     val termColumnsOpt: Option[Int] = Try { captureOutputInheritInput("stty", "size").split(" ")(1).trim.toInt }.toOption
+
+    val termColumns: Int = inConsoleOrElse(termColumnsOpt, None).getOrElse(78)
+
+    val RESET_COLOR: String = inConsoleOrElse("\u001b[0m", "")
+    val ERROR_COLOR: String = inConsoleOrElse("\u001b[91m", "")
+    val INFO_COLOR: String = inConsoleOrElse("\u001b[92m", "")
+    val DEBUG_COLOR: String = inConsoleOrElse("\u001b[93m", "")
+    val GRAPH_COLOR: String = inConsoleOrElse("\u001b[94m", "")
+    val STRESS_COLOR: String = inConsoleOrElse("\u001b[95m", "")
+
+    def ERROR(s: String): String = ERROR_COLOR + s + RESET_COLOR
+    def INFO(s: String): String = INFO_COLOR + s + RESET_COLOR
+    def DEBUG(s: String): String = DEBUG_COLOR + s + RESET_COLOR
+    def GRAPH(s: String): String = GRAPH_COLOR + s + RESET_COLOR
+    def STRESS(s: String): String = STRESS_COLOR + s + RESET_COLOR
+
+    val TERM_SEPLINE = "=" * termColumns
+
+    def print_sep(): Unit = System.err.println(GRAPH(TERM_SEPLINE))
+
+    def print_info(vals: Any*): Unit = System.err.println(GRAPH(":::: ") + INFO(vals.mkString("").replace(RESET_COLOR, INFO_COLOR)))
+    def print_error(vals: Any*): Unit = System.err.println(GRAPH("!!!! ") + ERROR(vals.mkString("").replace(RESET_COLOR, ERROR_COLOR)))
+    
+    def print_debug(vals: Any*): Unit = {
+        if (verbose) {
+            System.err.println(GRAPH(":::: ... ") + DEBUG(vals.mkString("").replace(RESET_COLOR, DEBUG_COLOR)))
+        }
+    }
 }
     
 import AkScriptUtils._
@@ -31,8 +65,9 @@ class Conf(args: Seq[String]) extends ScallopConf(args) {
     
     override def onError(e: Throwable): Unit = e match {
         case RequiredOptionNotFound(optionName) =>
-            System.err.println(s"\n\nRequired option not found: ${optionName}\nSee below for more info...\n\n")
+            print_error(s"\n\nRequired option not found: ${STRESS(optionName)}\nSee below for more info...\n\n")
             printHelp()
+            println()
             System.exit(-1)
     }
         
@@ -41,7 +76,9 @@ class Conf(args: Seq[String]) extends ScallopConf(args) {
     
 object AkGCodeApp extends App {
     val conf = new Conf(args)
+
+    print_sep()
     
-    println(termColumnsOpt)
+    println((termColumns))
     println((conf.inputFilename, conf.verbose))
 }
