6f264e29ccb3824af2e46146acbae185ce16a502
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-07-18 16:53:12 +0200

    blah

--- akgcode.scala
+++ akgcode.scala
@@ -1,14 +1,27 @@
-import java.nio.file
-
-import org.rogach.scallop._
+import org.rogach.scallop.{ScallopConf, ScallopOption}
+import org.rogach.scallop.exceptions.RequiredOptionNotFound
 
 class Conf(args: Seq[String]) extends ScallopConf(args) {
-    val inputFilename = trailArg[String]()
+    version("akgcode by Akshaal, 2017")
+    banner("""Usage: akgcode [OPTION]... <input-filename>
+             |Options:
+             |""".stripMargin)
+        
+    val verbose: ScallopOption[Boolean] = opt(descr = "Show verbose messages")
+    val inputFilename: ScallopOption[String] = trailArg(descr = "GCode file to process")
+    
+    override def onError(e: Throwable): Unit = e match {
+        case RequiredOptionNotFound(optionName) =>
+            System.err.println(s"\n\nRequired option not found: ${optionName}\nSee below for more info...\n\n")
+            printHelp()
+            System.exit(-1)
+    }
+        
     verify()
 }
-    
+
 object AkGCodeApp extends App {
     val conf = new Conf(args)
-    
-    println(conf.inputFilename.get)
+
+    println((conf.inputFilename, conf.verbose))
 }
