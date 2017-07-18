a87662b7191e350b3be10c1a8079710fc87d5ed7
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-07-18 16:23:37 +0200

    blah

--- akgcode.scala
+++ akgcode.scala
@@ -1,1 +1,14 @@
-println("Hello world!")
+import java.nio.file
+
+import org.rogach.scallop._
+
+class Conf(args: Seq[String]) extends ScallopConf(args) {
+    val inputFilename = trailArg[String]()
+    verify()
+}
+    
+object AkGCodeApp extends App {
+    val conf = new Conf(args)
+    
+    println(conf.inputFilename.get)
+}
